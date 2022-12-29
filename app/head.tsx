a7e1fb48deb5b8d7856ae587dd5acae6b44cedc8
Agus Moles <agusmoles@gmail.com>
2022-12-29 14:07:52 -0300

    Initial setup of app directory, images and background

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>branuelcs</title>
+      <meta
+        name="description"
+        content="BranuelCS Jugador profesional de CSGO"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.png" />
+    </>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
