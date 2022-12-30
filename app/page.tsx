73d58cfb2e3e772efe5e81e937c9771006e0064b
Agus Moles <agusmoles@gmail.com>
2022-12-30 15:18:27 -0300

    Adds sponsors and main card

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className={styles.container}>
-      asd
       <Image
         className={styles.branuelImage}
         src="/images/branuel.png"
@@ -31,6 +30,42 @@ export default function Home() {
         width={1920}
         height={1080}
       />
+      <Image
+        className={styles.argFlag}
+        src="/images/arg-flag.png"
+        alt="Bandera argentina"
+        width={800}
+        height={475}
+      />
+      <h1 className={inter.className}>BRANUEL</h1>
+
+      <section className={styles.info}>
+        <article className={styles.card}>Info</article>
+      </section>
+
+      <section className={styles.sponsors}>
+        <Image
+          className={styles.csgofast}
+          src="/images/sponsors/csgofast.png"
+          alt="CSGOFast"
+          width={182}
+          height={227}
+        />
+        <Image
+          className={styles.fastskins}
+          src="/images/sponsors/fastskins.png"
+          alt="Fastskins"
+          width={319}
+          height={111}
+        />
+        <Image
+          className={styles.hellcase}
+          src="/images/sponsors/hellcase.png"
+          alt="Hellcase"
+          width={182}
+          height={324}
+        />
+      </section>
     </div>
   );
 }
